321b79186ae107d5c2b922996992ea4edda1a603
rempartIrien <rempart.irien@gmail.com>
2022-08-18 00:31:42 +0200

    Add GraphQL code generation

---
+++ generate-graphql-types.js
@@ -0,0 +1,25 @@
+const { generate } = require("@graphql-codegen/cli");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const config = {
+  overwrite: true,
+  schema: process.env.API,
+  documents: "./app/**/*.ts",
+  generates: {
+    "app/graphql/generated/types.d.ts": {
+      plugins: [
+        {
+          add: {
+            content: "// This file is generated: do not edit it manually!",
+          },
+        },
+        "typescript",
+        "typescript-operations",
+      ],
+    },
+  },
+};
+
+generate(config, true);
