bcb171fdf1b5bca31d6ea6a1f2bd7b481276067e
rempartIrien <rempart.irien@gmail.com>
2022-11-13 00:48:23 +0100

    Add fonts

---
+++ src/routes/(home).css.ts
@@ -0,0 +1,13 @@
+import { style } from "@vanilla-extract/css";
+
+export const titleStyle = style({
+  fontFamily: "'Zen Old Mincho', serif",
+  fontSize: "48px",
+  fontWeight: "bold",
+});
+
+export const paragraphStyle = style({
+  fontFamily:
+    "'Zen Kaku Gothic New', 'Open Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "16px",
+});
--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -1,9 +1,16 @@
+import { paragraphStyle, titleStyle } from "./(home).css";
 import "./font.css";
 
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Easy Gundam!</h1>
+      <h1 class={titleStyle}>
+        Welcome to Easy Gundam! (イジ士ガンダムにようこそ)
+      </h1>
+      <p class={paragraphStyle}>
+        This site speaks about Mobile Suit Gundam (機動戦士ガンダム), a series
+        by Yoshiyuki Tomino (富野 由悠季 or 富野 喜幸).
+      </p>
     </main>
   );
 }
