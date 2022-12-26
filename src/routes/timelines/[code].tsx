cb1064ce2b62f92255181b4a66ae042d29d9fbc5
rempartIrien <rempart.irien@gmail.com>
2022-12-26 19:59:02 +0100

    Use nav component for in-page navigation

--- src/routes/timelines/[code].tsx
+++ src/routes/timelines/[code].tsx
@@ -1,9 +1,11 @@
 import { marked } from "marked";
 import { Show } from "solid-js";
 import type { RouteDataArgs } from "solid-start";
-import { A, Outlet, useRouteData } from "solid-start";
+import { Outlet, useRouteData } from "solid-start";
 import { createServerData$ } from "solid-start/server";
 
+import Nav from "~/components/Nav";
+import NavItem from "~/components/NavItem";
 import Title from "~/components/Title";
 import { getTimelineByCode } from "~/graphql/timeline.server";
 import { getLocale } from "~/i18n/i18n.cookie";
@@ -29,16 +31,14 @@ export default function Timelines() {
     <Show when={timeline()}>
       <section>
         <Title>{timeline()?.name}</Title>
-        <nav>
-          <ol>
-            <li>
-              <A href={`.`}>Description</A>
-            </li>
-            <li>
-              <A href={`series`}>Series</A>
-            </li>
-          </ol>
-        </nav>
+        <Nav
+          items={[
+            <NavItem href="" end>
+              Description
+            </NavItem>,
+            <NavItem href="series">Series</NavItem>,
+          ]}
+        />
         <Outlet />
       </section>
     </Show>
