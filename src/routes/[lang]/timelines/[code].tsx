71806486bdf05c7dbf29d14403c4a0c3e1f48359
rempartIrien <rempart.irien@gmail.com>
2023-01-07 17:29:01 +0100

    Use tiny-invariant

--- src/routes/[lang]/timelines/[code].tsx
+++ src/routes/[lang]/timelines/[code].tsx
@@ -4,6 +4,7 @@ import { Show } from "solid-js";
 import type { RouteDataArgs } from "solid-start";
 import { Outlet, useRouteData } from "solid-start";
 import { createServerData$ } from "solid-start/server";
+import invariant from "tiny-invariant";
 
 import Nav from "~/components/Nav";
 import NavItem from "~/components/NavItem";
@@ -12,6 +13,7 @@ import { getTimelineByCode } from "~/graphql/timeline.server";
 import { getLocale } from "~/i18n/i18n.cookie";
 
 export function routeData({ params }: RouteDataArgs) {
+  invariant(params.code, "Expected params.code");
   const timeline = createServerData$(
     async ([, code]: string[], { request }) => {
       const locale = await getLocale(request);
