b3ef1da722562278210c1efbbcb3b90cdcc06f1f
rempartIrien <rempart.irien@gmail.com>
2023-01-07 20:03:40 +0100

    Keep locale in links to home

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,14 +1,19 @@
 import { useI18n } from "@solid-primitives/i18n";
+import { useContext } from "solid-js";
+import { A } from "solid-start";
+
+import { LocaleContext } from "~/contexts/LocaleContext";
 
 export default function ErrorPage() {
   const [t] = useI18n();
+  const locale = useContext(LocaleContext);
 
   // TODO: HttpStatus according to kind of error (make two Error classes)
   return (
     <main>
       <h1>{t("error.title")}</h1>
       <p>
-        <a href="/">{t("error.links.backToHome")}</a>
+        <A href={locale ? `/${locale}` : "/"}>{t("error.links.backToHome")}</A>
       </p>
     </main>
   );
--- src/routes/[lang]/[...404].tsx
+++ src/routes/[lang]/[...404].tsx
@@ -1,14 +1,23 @@
 import { useI18n } from "@solid-primitives/i18n";
+import { useContext } from "solid-js";
+import { A } from "solid-start";
 import { HttpStatusCode } from "solid-start/server";
+
+import Title from "~/components/Title";
+import { LocaleContext } from "~/contexts/LocaleContext";
 
 export default function NotFound() {
   const [t] = useI18n();
+  const locale = useContext(LocaleContext);
+
   return (
     <main>
       <HttpStatusCode code={404} />
-      <h1>{t("notFound.title")}</h1>
+      <Title>{t("notFound.title")}</Title>
       <p>
-        <a href="/">{t("notFound.links.backToHome")}</a>
+        <A href={locale ? `/${locale}` : "/"}>
+          {t("notFound.links.backToHome")}
+        </A>
       </p>
     </main>
   );
