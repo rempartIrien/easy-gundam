aaa4e9acb6b846293e681d52c1ff5865794bcfc1
rempartIrien <rempart.irien@gmail.com>
2023-01-13 21:00:38 +0100

    Split Vite and Vitest confs

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
+/// <reference types="vitest" />
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [vanillaExtractPlugin(), solid(), tsconfigPaths()],
-	test: {
-		environment: "jsdom",
-		setupFiles: ["./test/setup-test-env.ts"],
-		globals: true,
-		transformMode: {
-			web: [/\.tsx?$/],
-		},
-		coverage: {
-			reporter: ["json", "html"],
-		},
-	},
-	resolve: {
-		conditions: ["development", "browser"],
-	},
+	plugins: [tsconfigPaths(), vanillaExtractPlugin(), solid()],
 });
---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+
+import viteConf from "./vite.config";
+
+// We need to use separate config files for Vite and Vitest due to the use of
+// `resolve.conditions` that are not compatible between dev and test builds.
+export default defineConfig({
+	...(await viteConf),
+	test: {
+		environment: "jsdom",
+		setupFiles: ["./test/setup-test-env.ts"],
+		globals: true,
+		transformMode: {
+			web: [/\.tsx?$/],
+		},
+		coverage: {
+			reporter: ["json", "html"],
+		},
+	},
+	resolve: {
+		conditions: ["development", "browser"],
+	},
+});
