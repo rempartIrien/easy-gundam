ca1301f851f82057a17ca457645f1dd6e6e55fbd
rempartIrien <rempart.irien@gmail.com>
2023-02-12 14:17:00 +0100

    Add some CSS transition

--- src/components/NavItem/NavItem.css.ts
+++ src/components/NavItem/NavItem.css.ts
@@ -3,8 +3,13 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "~/theme/theme.css";
 
 export const navItemStyle = style({
+	display: "inline-block",
 	textDecoration: "none",
 	color: vars.color.text.main,
+	borderBottomWidth: "4px",
+	borderBottomStyle: "solid",
+	borderBottomColor: "transparent",
+	transition: "0.2s ease border-bottom-color",
 	selectors: {
 		"&:hover": {
 			color: vars.color.primary.main,
@@ -13,5 +18,5 @@ export const navItemStyle = style({
 });
 
 export const navItemActiveClass = style({
-	borderBottom: `4px solid ${vars.color.primary.main} `,
+	borderBottomColor: vars.color.primary.main,
 });
