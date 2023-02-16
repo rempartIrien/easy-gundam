d416979c6eb438454e96909143a11d19359dc0ec
rempartIrien <rempart.irien@gmail.com>
2023-02-16 18:25:13 +0100

    Add Kobalte

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [tsconfigPaths(), vanillaExtractPlugin(), solid()],
+	ssr: {
+		noExternal: ["@kobalte/core"],
+	},
 });
