6b4deb14dd06168a704a0380452c1470487f65e9
rempartIrien <rempart.irien@gmail.com>
2023-03-18 22:26:01 +0100

    Add about page

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Next steps:
 
 - define layout
 - rework header
-- write About page content
 - allow dynamic component for Text and maybe Heading
 - switch to `@graphql-codegen/client-preset`
+- Rework the way markdown strings are rendered
 - use [lucide-solid](https://github.com/lucide-icons/lucide/tree/main/packages/lucide-solid) once [https://github.com/vitejs/vite/issues/8237](https://github.com/vitejs/vite/issues/8237) is solved
---
+++ src/routes/[lang]/about.tsx
@@ -0,0 +1,92 @@
+import { useI18n } from "@solid-primitives/i18n";
+
+import Heading from "~/components/Heading";
+import Link from "~/components/Link";
+import List from "~/components/List";
+import PageContent from "~/components/PageContent";
+import Paragraph from "~/components/Paragraph";
+import Text from "~/components/Text";
+
+export default function About() {
+	const [t] = useI18n();
+	const appName = t("appName");
+	return (
+		<PageContent title={t("about.title")}>
+			{/* Legal informations */}
+			<Heading variant="subtitle">{t("about.legal.title")}</Heading>
+			<Paragraph>{t("about.legal.texts.website", { appName })}</Paragraph>
+			<Paragraph>{t("about.legal.texts.gundam", { appName })}</Paragraph>
+			<Paragraph>{t("about.legal.texts.copyright", { appName })}</Paragraph>
+			<Paragraph>{t("about.legal.texts.texts", { appName })}</Paragraph>
+			<Paragraph>{t("about.legal.texts.media", { appName })}</Paragraph>
+			<List>
+				<li>
+					<Link href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000037388886/2020-06-16">
+						{t("about.legal.resources.fairUse")}
+					</Link>
+				</li>
+				<li>
+					<Link href="https://creativecommons.org/licenses/by-nc-nd/4.0">
+						{t("about.legal.resources.license")}
+					</Link>
+				</li>
+			</List>
+			{/* Privacy and data */}
+			<Heading variant="subtitle">{t("about.privacy.title")}</Heading>
+			<Paragraph>
+				{t("about.privacy.texts.cookies.list", { appName })}
+			</Paragraph>
+			<List>
+				<li>
+					<Text>{t("about.privacy.texts.cookies.theme")}</Text>
+				</li>
+				<li>
+					<Text>{t("about.privacy.texts.cookies.language")}</Text>
+				</li>
+			</List>
+			<Paragraph>
+				{t("about.privacy.texts.cookies.expiration", { appName })}
+			</Paragraph>
+			<Paragraph>
+				{t("about.privacy.texts.cookies.noCookies", { appName })}
+			</Paragraph>
+			<Paragraph>{t("about.privacy.texts.dataSharing", { appName })}</Paragraph>
+			<List>
+				<li>
+					<Link href="../cookies">
+						{t("about.privacy.resources.cookieManagement")}
+					</Link>
+				</li>
+				<li>
+					<Link href="https://gdpr.eu">
+						{t("about.privacy.resources.gdpr")}
+					</Link>
+				</li>
+			</List>
+			{/* Website purpose */}
+			<Heading variant="subtitle">{t("about.purpose.title")}</Heading>
+			<Paragraph>{t("about.purpose.texts.share", { appName })}</Paragraph>
+			<Paragraph>
+				{t("about.purpose.texts.exhaustiveness", { appName })}
+			</Paragraph>
+			<Paragraph>{t("about.purpose.texts.free", { appName })}</Paragraph>
+			<List>
+				<li>
+					<Link href="http://www.aeug.fr">
+						{t("about.purpose.resources.aeug")}
+					</Link>
+				</li>
+				<li>
+					<Link href="https://gundam-france.com">
+						{t("about.purpose.resources.gundamFrance")}
+					</Link>
+				</li>
+				<li>
+					<Link href="https://gundam.fandom.com/wiki/The_Gundam_Wiki">
+						{t("about.purpose.resources.wiki")}
+					</Link>
+				</li>
+			</List>
+		</PageContent>
+	);
+}
