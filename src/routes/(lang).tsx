435cd1c9af15222cd7f6854a4a05b9af755a9e85
rempartIrien <rempart.irien@gmail.com>
2023-03-26 16:22:10 +0200

    Fix first redirection

--- src/routes/(lang).tsx
+++ src/routes/(lang).tsx
@@ -1,19 +1,14 @@
 import { Show } from "solid-js";
-import type { RouteDataArgs } from "solid-start";
 import { useRouteData } from "solid-start";
 import { createServerData$, redirect } from "solid-start/server";
-import invariant from "tiny-invariant";
 
-import type { Language } from "~/i18n/i18n.config";
+import { getLocale } from "~/i18n/i18n.cookie";
 
-export function routeData({ params }: RouteDataArgs) {
-	invariant(params.lang, "Expected params.lang");
-	return createServerData$(
-		([locale]) => {
-			throw redirect(`/${locale as Language}/home`);
-		},
-		{ key: [params.lang] },
-	);
+export function routeData() {
+	return createServerData$(async (_, { request }) => {
+		const locale = await getLocale(request);
+		throw redirect(`/${locale}/home`);
+	});
 }
 
 export default function Index() {
