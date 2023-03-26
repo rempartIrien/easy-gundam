79b4d1a0717e0bf6c383459b3bb50f0a7cd2c894
rempartIrien <rempart.irien@gmail.com>
2023-03-26 19:39:11 +0200

    Use graphql-codegen client-preset package

--- README.md
+++ README.md
@@ -2,9 +2,5 @@
 
 Next steps:
 
-- define layout
-- rework header
-- allow dynamic component for Text and maybe Heading
-- switch to `@graphql-codegen/client-preset`
 - Rework the way markdown strings are rendered
 - use [lucide-solid](https://github.com/lucide-icons/lucide/tree/main/packages/lucide-solid) once [https://github.com/vitejs/vite/issues/8237](https://github.com/vitejs/vite/issues/8237) is solved
--- codegen.ts
+++ codegen.ts
@@ -1,15 +1,20 @@
 // eslint-disable-next-line import/named
-import { generate } from "@graphql-codegen/cli";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+if (!process.env.VITE_API) {
+	throw new Error(
+		"Env variable VITE_API should be defined to generate GraphQL types",
+	);
+}
 
 const config = {
 	overwrite: true,
 	schema: `${process.env.VITE_API}/graphql`,
 	documents: "./src/graphql/**/*.ts",
 	generates: {
-		"src/graphql/generated/types.d.ts": {
+		["src/graphql/generated/types.d.ts"]: {
 			plugins: [
 				{
 					add: {
@@ -26,4 +31,4 @@ const config = {
 	},
 };
 
-generate(config, true);
+export default config;
