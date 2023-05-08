8c6dd44d2a5cd5dc94925610a65abd5cdaf47e17
rempartIrien <rempart.irien@gmail.com>
2023-05-08 18:34:25 +0200

    Add menu icon

--- src/components/Icon/icon-registry.ts
+++ src/components/Icon/icon-registry.ts
@@ -3,6 +3,7 @@ import Error from "../../../assets/icons/error.svg";
 import ExternalLink from "../../../assets/icons/external-link.svg";
 import Info from "../../../assets/icons/info.svg";
 import Languages from "../../../assets/icons/languages.svg";
+import Menu from "../../../assets/icons/menu.svg";
 import Moon from "../../../assets/icons/moon.svg";
 import Success from "../../../assets/icons/success.svg";
 import Sun from "../../../assets/icons/sun.svg";
@@ -14,6 +15,7 @@ const ICON_REGISTRY = {
 	externalLink: ExternalLink,
 	info: Info,
 	languages: Languages,
+	menu: Menu,
 	moon: Moon,
 	success: Success,
 	sun: Sun,
