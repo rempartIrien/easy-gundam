4460bef4e776a4f7ad6c4f5dc1ec0f2c48ebe3e1
rempartIrien <rempart.irien@gmail.com>
2023-05-18 19:33:25 +0200

    Redirect on wrong language

--- src/routes/[lang].tsx
+++ src/routes/[lang].tsx
@@ -1,9 +1,14 @@
-import { Outlet } from "solid-start";
+import { Show } from "solid-js";
+import type { RouteDataArgs } from "solid-start";
+import { Outlet, useRouteData } from "solid-start";
+import { createServerData$, redirect } from "solid-start/server";
 
 import CenterContent from "~/components/CenterContent";
 import DocumentTitle from "~/components/DocumentTitle";
 import Footer from "~/components/Footer";
 import Header from "~/components/Header";
+import { LanguageNmes } from "~/i18n/i18n.config";
+import { getLocale } from "~/i18n/i18n.cookie";
 
 import {
 	containerStyle,
@@ -12,15 +17,33 @@ import {
 	headerStyle,
 } from "./[lang].css";
 
+export function routeData({ params }: RouteDataArgs) {
+	return createServerData$(
+		async ([lang], { request }) => {
+			const locale = await getLocale(request);
+			if (!Object.keys(LanguageNmes).includes(lang)) {
+				throw redirect(`/${locale}/home`);
+			}
+			return true;
+		},
+		{ key: [params.lang] },
+	);
+}
+
 export default function I18nLayout() {
+	// Need to call data in JSX. See https://github.com/solidjs/solid-start/issues/577
+	const data = useRouteData<typeof routeData>();
+
 	return (
-		<section class={containerStyle}>
-			<DocumentTitle />
-			<Header class={headerStyle} />
-			<CenterContent class={contentStyle}>
-				<Outlet />
-			</CenterContent>
-			<Footer class={footerStyle} />
-		</section>
+		<Show when={data()}>
+			<section class={containerStyle}>
+				<DocumentTitle />
+				<Header class={headerStyle} />
+				<CenterContent class={contentStyle}>
+					<Outlet />
+				</CenterContent>
+				<Footer class={footerStyle} />
+			</section>
+		</Show>
 	);
 }
