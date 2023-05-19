125790b6f24c8144bc5e93fef1559cd5ee7de157
rempartIrien <rempart.irien@gmail.com>
2023-05-19 21:51:43 +0200

    Use system font stacks

--- src/theme/theme.css.ts
+++ src/theme/theme.css.ts
@@ -77,40 +77,58 @@ const cerulean = "#0492C2";
 const ceruleanLight = "#d1f3fe";
 const ceruleanDark = "#023a4e";
 
+const emojiFontStack =
+	"'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'";
+
+// See https://github.com/system-fonts/modern-font-stacks#geometric-humanist
+const sansSerifFontStack =
+	`system-ui, Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif, ${emojiFontStack}` as const;
+
+// See https://github.com/system-fonts/modern-font-stacks#didone
+const serifFontStack =
+	`Didot, 'Bodoni MT', 'Noto Serif Display', 'URW Palladio L', P052, Sylfaen, serif, ${emojiFontStack}` as const;
+
+// See https://github.com/system-fonts/modern-font-stacks#monospace-code
+const monoFontStack =
+	`ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace, ${emojiFontStack}` as const;
+
+type FontFamily =
+	| typeof sansSerifFontStack
+	| typeof serifFontStack
+	| typeof monoFontStack;
+
 interface FontVariables {
-	family: "M PLUS 1p";
+	family: FontFamily;
 	size: `${number}rem`;
 	lineHeight: `${number}`;
 }
-
-const fontFamiliySansSerif = "M PLUS 1p";
 
 const font: Record<
 	"title" | "subtitle" | "bigText" | "normalText" | "smallText",
 	FontVariables
 > = {
 	title: {
-		family: fontFamiliySansSerif,
+		family: sansSerifFontStack,
 		size: "4rem",
 		lineHeight: "1.3",
 	},
 	subtitle: {
-		family: fontFamiliySansSerif,
+		family: sansSerifFontStack,
 		size: "2rem",
 		lineHeight: "1.35",
 	},
 	bigText: {
-		family: fontFamiliySansSerif,
+		family: sansSerifFontStack,
 		size: "1.25rem",
 		lineHeight: "1.6",
 	},
 	normalText: {
-		family: fontFamiliySansSerif,
+		family: sansSerifFontStack,
 		size: "1rem",
 		lineHeight: "1.6",
 	},
 	smallText: {
-		family: fontFamiliySansSerif,
+		family: sansSerifFontStack,
 		size: "0.75rem",
 		lineHeight: "1.6",
 	},
