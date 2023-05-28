724d4b013a370dd495ad115133d7729745ece147
rempartIrien <rempart.irien@gmail.com>
2023-05-28 15:56:08 +0200

    Adapt List text styles

--- src/components/List/List.css.ts
+++ src/components/List/List.css.ts
@@ -2,9 +2,13 @@ import { style } from "@vanilla-extract/css";
 
 import { textBlockStyles } from "~/theme/styles.css";
 import { relativeSpace } from "~/theme/utils";
+import vars from "~/theme/variables.css";
 
 export const listStyle = style({
 	...textBlockStyles,
 	marginBottom: relativeSpace(2),
 	paddingLeft: relativeSpace(4),
+	fontSize: vars.font.normalText.size,
+	fontFamily: vars.font.normalText.family,
+	lineHeight: vars.font.normalText.lineHeight,
 });
