4a6fe786add82e4b5c29347a171f55a7872b2bef
rempartIrien <rempart.irien@gmail.com>
2023-08-15 13:22:31 +0200

    Update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,14 +6,12 @@ import viteConf from "./vite.config";
 // We need to use separate config files for Vite and Vitest due to the use of
 // `resolve.conditions` that are not compatible between dev and test builds.
 export default defineConfig({
+	// eslint-disable-next-line @typescript-eslint/await-thenable
 	...(await viteConf),
 	test: {
 		environment: "jsdom",
 		setupFiles: ["./test/setup-test-env.ts"],
 		globals: true,
-		transformMode: {
-			web: [/\.tsx?$/],
-		},
 		coverage: {
 			reporter: ["json", "html"],
 		},
