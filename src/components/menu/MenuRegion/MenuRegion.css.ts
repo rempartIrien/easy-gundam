e442d47ca469cf2ae5feec88e74f6fb5435475e7
rempartIrien <rempart.irien@gmail.com>
2024-06-08 16:01:25 +0200

    Fix menu z-index

--- src/components/menu/MenuRegion/MenuRegion.css.ts
+++ src/components/menu/MenuRegion/MenuRegion.css.ts
@@ -2,5 +2,5 @@ import { style } from "@vanilla-extract/css";
 
 export const menuRegionStyle = style({
 	position: "relative",
-	zIndex: 1, // Be above header that also has a zIndex
+	zIndex: 2, // Be above header that also has a zIndex
 });
