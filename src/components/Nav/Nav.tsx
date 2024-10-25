d8612cbce40562ad1002d45114684519aea68c9a
rempartIrien <rempart.irien@gmail.com>
2024-10-25 11:32:17 +0200

    Remove unneeded gap

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import type { JSX } from "solid-js";
-import { For, splitProps } from "solid-js";
+import { For, createMemo, splitProps } from "solid-js";
 
 import type NavItem from "../NavItem";
 
@@ -11,6 +11,7 @@ type NavProps = Omit<JSX.HTMLAttributes<HTMLElement>, "class" | "children"> & {
 
 export default function Nav(props: NavProps) {
 	const [local, others] = splitProps(props, ["items", "vertical"]);
+	const items = createMemo(() => local.items.filter((v) => !!v));
 
 	return (
 		<nav {...others}>
@@ -20,7 +21,7 @@ export default function Nav(props: NavProps) {
 					local.vertical ? "flex-col px-1r py-1f" : "p-0",
 				])}
 			>
-				<For each={local.items}>{(item) => <li>{item}</li>}</For>
+				<For each={items()}>{(item) => <li>{item}</li>}</For>
 			</ol>
 		</nav>
 	);
